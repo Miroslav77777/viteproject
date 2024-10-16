ddd5446593c9c6333fe762540318f8c5588a9445
Miroslav <152916320+Miroslav77777@users.noreply.github.com>
2024-10-16 10:06:39 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Outlet, HashRouter} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet} from "react-router-dom";
 import React, { useState } from 'react';
 import Home from './pages/Home/Home';
 import AboutUs from './pages/AboutUs/AboutUs';
@@ -9,13 +9,13 @@ function App() {
   const [showMain, setShowMain] = useState(true);
 
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>  
       <Route path="viteproject" element={<Home/>} /> 
       <Route path="/viteproject/about" element={<AboutUs/>} /> 
       <Route path='/viteproject/pricelist' element={<PriceList />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
