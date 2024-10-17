069b9e5dcd52e04ca80b707e7f0ca45a8cdad9b7
Miroslav77777 <crazy11032005@gmail.com>
2024-10-17 21:16:21 +0300

    routing trying

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import './App.css';
-import { HashRouter, Routes, Route, Outlet} from "react-router-dom";
-import React, { useState } from 'react';
-import Home from './pages/Home/Home';
-import AboutUs from './pages/AboutUs/AboutUs';
-import PriceList from './pages/PriceList/PriceList';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home"; // Import correctly
+import AboutUs from "./pages/AboutUs/AboutUs";
+import PriceList from "./pages/PriceList/PriceList";
 
 function App() {
-  const [showMain, setShowMain] = useState(true);
-
   return (
-    <HashRouter> 
+    <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/about" element={<AboutUs />} /> 
-        <Route path='/pricelist' element={<PriceList />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/pricelist" element={<PriceList />} />
       </Routes>
     </HashRouter>
   );
