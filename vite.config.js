0b58bdbef820ad5d124ee97c8621fc4fb951ec01
Miroslav77777 <crazy11032005@gmail.com>
2024-10-23 18:39:14 +0300

    ya zadolbalsa

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/",
+  build: {
+    outDir: 'dist', // Указываем папку для сборки
+    publicPath: '/' // Устанавливаем публичный путь в корень
+  }
 })
