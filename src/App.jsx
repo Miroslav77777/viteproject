b051bb49ec70ef0a61559b981be676970a9a9818
Miroslav77777 <crazy11032005@gmail.com>
2024-10-23 18:20:41 +0300

    routt

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
-import Home from "./pages/Home/Home"; // Import correctly
+import { HashRouter, Routes, Route } from "react-router-dom"; 
+import Home from "./pages/Home/Home"; 
 import AboutUs from "./pages/AboutUs/AboutUs";
 import PriceList from "./pages/PriceList/PriceList";
-import { createBrowserHistory } from 'history';
-
-const history = createBrowserHistory();
 
 function App() {
   return (
-    <Router history={history}>
-      <BrowserRouter basename="/viteproject">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/pricelist" element={<PriceList />} />
-        </Routes>
-      </BrowserRouter>
-    </Router>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/pricelist" element={<PriceList />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
