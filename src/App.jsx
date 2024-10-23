f626d66ce9d33e94083b6133d2f015ac7a946148
Miroslav77777 <crazy11032005@gmail.com>
2024-10-23 17:54:26 +0300

    envo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Provider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home"; // Import correctly
 import AboutUs from "./pages/AboutUs/AboutUs";
 import PriceList from "./pages/PriceList/PriceList";
@@ -6,15 +6,13 @@ import PriceList from "./pages/PriceList/PriceList";
 
 function App() {
   return (
-    <Provider store = {store}>
-      <BrowserRouter basename="/viteproject">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/pricelist" element={<PriceList />} />
-        </Routes>
-      </BrowserRouter>
-      </Provider>
+    <BrowserRouter basename="/viteproject">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/pricelist" element={<PriceList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
