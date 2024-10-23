822f575871066530074ea3c90f8fbddd251f3121
Miroslav77777 <crazy11032005@gmail.com>
2024-10-23 18:16:41 +0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import Home from "./pages/Home/Home"; // Import correctly
 import AboutUs from "./pages/AboutUs/AboutUs";
 import PriceList from "./pages/PriceList/PriceList";
+import { createBrowserHistory } from 'history';
 
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <BrowserRouter basename="/viteproject">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/pricelist" element={<PriceList />} />
-      </Routes>
-    </BrowserRouter>
+    <Router history={history}>
+      <BrowserRouter basename="/viteproject">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/pricelist" element={<PriceList />} />
+        </Routes>
+      </BrowserRouter>
+    </Router>
   );
 }
 
